b07d9c8bb3d779f2a4da83f5237cc7f8d4be887e
keroken <kenichi.shinagawa@gmail.com>
2021-09-26 13:09:42 +0900

    add Provider to App and start creating RepositoriesList

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../state/store';
+import RepositoriesList from './RepositoriesList';
 
 const App = () => {
   return (
-    <div>
-      <h1>Search For a Package</h1>
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1>Search For a Package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
   );
 };
 
